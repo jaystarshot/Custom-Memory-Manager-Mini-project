e87be931010e5e6e4d1d15005193c8996acd0110
Jay Narale <jaynarale3479@gmail.com>
2017-11-08 22:27:52 +0530

    Create DefaultMemManager.cpp

---
+++ DefaultMemManager.cpp
@@ -0,0 +1,31 @@
+ #include <sys/types.h> 
+#include <bits/stdc++.h>
+
+using namespace std;
+class Complex 
+  {
+  public:
+    Complex (double a, double b): r (a), c (b) {}
+   // inline void* operator new(size_t);
+    //inline void   operator delete(void*);
+  private:
+    double r; // Real Part
+    double c; // Complex Part
+  };
+
+ int main(int argc, char* argv[]) 
+  {
+    auto t = clock();
+  Complex* array[1000];
+  for (int i = 0;i  <  5000; i++) {
+    for (int j = 0; j  <  1000; j++) {
+      array[j] = new Complex (i, j);
+      }
+    for (int j = 0; j  <  1000; j++) {
+      delete array[j];
+      }
+    }
+    t = clock()-t;
+    cout<<"It took us "<<((float)t)/CLOCKS_PER_SEC<<" secs"<<endl;
+  return 0;
+  }
